3f61b0ec128721021bf76a9bc9bef7512eed4e03
Faham <mahmoud.faham@mobilar.io>
2023-08-24 22:52:12 +0300

    filter and search

--- src/app/modules/store/store.component.ts
+++ src/app/modules/store/store.component.ts
@@ -1,11 +1,16 @@
 import { NgForOf, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { Title } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
-import { Product, ProductsService } from 'src/@api';
+import { CategoriesService, Product, ProductsService } from 'src/@api';
 import { ProductCardComponent } from 'src/app/core/components/product-card/product-card.component';
 
 @Component({
@@ -13,16 +18,19 @@ import { ProductCardComponent } from 'src/app/core/components/product-card/produ
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss'],
   standalone: true,
-  imports: [MatSnackBarModule, NgIf, MatButtonModule, MatIconModule, NgForOf, ProductCardComponent]
+  imports: [MatSnackBarModule, FormsModule, MatFormFieldModule, NgIf, MatButtonModule, MatInputModule, MatSelectModule, MatIconModule, NgForOf, ProductCardComponent, MatPaginatorModule]
 })
 export class StoreComponent implements OnInit {
 
   products: Product[];
   filteredProducts: Product[];
+  categories: string[] = [];
+  search: string;
 
   constructor(
     protected _t: Title,
-    private productsService: ProductsService,
+    private _productsService: ProductsService,
+    private _categoriesService: CategoriesService,
     private _snackBar: MatSnackBar,
   ) {
     _t.setTitle('Store');
@@ -30,23 +38,44 @@ export class StoreComponent implements OnInit {
 
   ngOnInit() {
     this.getProductList();
+    this.getCategories();
   }
 
 
 
   getProductList() {
-    this._getProducts(this.productsService.getProducts());
+    this._getProducts(this._productsService.getProducts());
   }
 
 
   public getProductsByCategory(category: string) {
-    this._getProducts(this.productsService.getProductsByCategory(category));
+    if (!category) {
+      this.getProductList();
+      return;
+    }
+
+    this._getProducts(this._productsService.getProductsByCategory(category));
   }
 
-  public searchProducts(searchTerm: string) {
+  public searchProducts() {
+    if (!this.search || !this.search.trim()) { this.filteredProducts = this.products; return; }
+
     this.filteredProducts = this.products.filter((product) => {
-      return product.title.toLowerCase().includes(searchTerm.toLowerCase());
+      return product.title.toLowerCase().includes(this.search.toLowerCase());
     });
+  }
+
+
+
+  getCategories() {
+    this._categoriesService.getCategories().subscribe(
+      (data) => {
+        this.categories = data;
+      },
+      () => {
+        this._snackBar.open('Something went wrong', 'Close', { duration: 3000 });
+      }
+    );
   }
 
 
@@ -54,7 +83,7 @@ export class StoreComponent implements OnInit {
     products.subscribe(
       (data) => {
         this.products = data;
-        this.filteredProducts = data;
+        this.searchProducts();
       },
       () => {
         this._snackBar.open('Something went wrong', 'Close', { duration: 3000 });
